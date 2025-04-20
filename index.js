9088b55cc77c832af35013a1caabf8c635f05a99
Tyiss Goodman <sticktyiss@gmail.com>
2025-04-19 18:51:21 -0700

    Added background radient coloring to 'main' section and further fixed heading scroll animation (turned position to fixed - it stops the juttering when scrolling)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.getElementById("siteHeader");
   window.addEventListener("scroll", function () {
-    if (window.scrollY > 100) {
+    if (window.scrollY > 70) {
       header.classList.add("shrink");
     } else {
       header.classList.remove("shrink");
