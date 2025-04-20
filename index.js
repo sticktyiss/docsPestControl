7204e3a027b0433ba88c4e2f4c26b4942613b7f1
Tyiss Goodman <sticktyiss@gmail.com>
2025-04-19 17:30:05 -0700

    Fixed scrolling animation to get rid of jumping issue.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.getElementById("siteHeader");
   window.addEventListener("scroll", function () {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 60) {
       header.classList.add("shrink");
     } else {
       header.classList.remove("shrink");
